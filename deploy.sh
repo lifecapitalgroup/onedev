8d8bb6c5f905c0b6b0cdbd2b0b8c765c74ef08f4
Mustafa Elmalah <mnav@fastmail.com>
2025-05-31 01:40:09 +0300

    build/deployment scripts; systemd unit file

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+target_path="/var/www/onedev-lifecapital"
+ver="11.9.9"
+
+# build assumed on same machine
+git pull \
+&& mvn package -Pce \
+&& pushd server-product/target/ \
+&& rm -rf onedev-$ver \
+&& unzip onedev-$ver.zip \
+&& rsync --progress --stats -rltgoD \
+  	onedev-$ver/* $target_path \
+	--exclude=hibernate.properties \
+	--rsync-path="sudo rsync" \
+&& popd \
+&& sudo systemctl restart onedev-lifecapital
