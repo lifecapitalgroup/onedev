b94dc93e016d9ef2a02cacebe91fbba9f728ca54
Mustafa Elmalah <mnav@fastmail.com>
2025-06-25 20:25:17 +0300

    build/run scripts; deploy.sh --> deploy-local.sh

---
+++ run-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p ./__prebuild__backup \
+	&& mv ./server-product/target/onedev-11.9.9/conf ./__prebuild__backup/conf \
+	&& mv ./server-product/target/onedev-11.9.9/internaldb ./__prebuild__backup/internaldb \
+	&& mv ./server-product/target/onedev-11.9.9/site/projects __prebuild__backup/projects \
+	&& rm -rf ./server-product/target \
+	&& mvn package -Pce \
+	&& pushd  ./server-product/target/ \
+	&& unzip -x ./onedev-11.9.9.zip \
+	&& rm -rf ./onedev-11.9.9/conf \
+	&& rm -rf ./onedev-11.9.9/internaldb \
+	&& rm -rf ./onedev-11.9.9/site/projects \
+	&& mv ../../__prebuild__backup/conf ./onedev-11.9.9/conf \
+	&& mv ../../__prebuild__backup/internaldb ./onedev-11.9.9/internaldb \
+	&& mv ../../__prebuild__backup/projects ./onedev-11.9.9/site/projects \
+	&& popd \
+	&& ./server-product/target/onedev-11.9.9/bin/server.sh console
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./server-product/target/onedev-11.9.9/bin/server.sh console
